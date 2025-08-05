ab456f3d223eaa3f104bb7ec00c2294e7c1d1036
Joel <jken223@aucklanduni.ac.nz>
2025-08-06 03:33:58 +1200

    feat: initial random forest classifier model

--- src/modules/ml/ClassChangeModel.py
+++ src/modules/ml/ClassChangeModel.py
@@ -9,6 +9,8 @@ class ClassChangeModel:
     def __init__(self):
         self.model = RandomForestClassifier(n_estimators=100, random_state=42)
         self.scaler = StandardScaler()
+        self.data_columns = []
+        self.cat_cols = []
         
     def change_buckets(change):
         match change:
@@ -25,8 +27,39 @@ class ClassChangeModel:
     
     def process_data(self, df):
         data_columns = [col for col in df.columns if col not in ['Class', 'Incoming Change']]
+        category_cols = [col for col in data_columns if col not in ['Total Dependencies', 'Class', 'Incoming Change']]
         X = df[data_columns]
         y = df['Incoming Change'].apply(self.change_buckets)
 
+        self.data_columns = data_columns
+        self.cat_cols = category_cols
+
         return X, y
+    
+    def train(self, tracker_df):
+
+        X_train, X_test, y_train, y_test = self.process_data(tracker_df)
+        self.model.fit(X_train, y_train)
+        y_pred = self.model.predict(X_test)
+
+        importance = pd.DataFrame({
+                'Characteristic': self.data_columns,
+                'Importance': self.model.feature_importances_
+            }).sort_values('Importance', ascending=False)
+            
+        print("\nCharacteristic Importance:")
+        print(importance)
+        
+        
+        category_importance = importance[
+            importance['Characteristic'].isin(self.cat_cols)
+        ]
+        print("\nCategory Influence on Changes:")
+        print(category_importance)
+        
+        return {
+            'classification_report': classification_report(y_test, y_pred),
+            'confusion_matrix': confusion_matrix(y_test, y_pred, labels=self.model.classes_),
+            'Characteristic_importance': importance
+        } 
     
